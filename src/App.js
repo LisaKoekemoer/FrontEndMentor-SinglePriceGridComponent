5c995b3ebe21546f22b41738695f44dbef0d04af
Alicia <alicia.koekemoer98@gmail.com>
2024-06-17 17:40:42 +0200

    Completed Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main-component'>
+        <div className='header-section'>
+          <h2>Join our community</h2>
+          <h3>30-day, hassle-free money back guarantee</h3>
+          <p>Gain access to our full library of tutorials along with expert code reviews. 
+            Perfect for any developers who are serious about honing their skills.</p>
+        </div>
+
+        <div className='community-info'>
+          <div className='subscription'>
+            <h2>Monthly Subscription</h2>
+            <div className='price'>
+              <h2>$29</h2>
+              <p>per month</p>
+            </div>
+            <p className='description'>Full access for less that $1 a day</p>
+            <button className='btn' type="submit">Sign Up</button>
+          </div>
+          
+          <div className='about'>
+            <h2>Why us</h2>
+            <ul>
+              <li>Tutorials by industry experts</li>
+              <li>Peer & expert review</li>
+              <li>Coding exercises</li>
+              <li>Access to our Github repos</li>
+              <li>Community forum</li>
+              <li>Flashcard decks</li>
+              <li>New videos every week</li>
+            </ul>
+          </div>
+        </div>   
+      </div>
+    </>
   );
 }
 
